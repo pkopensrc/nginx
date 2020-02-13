dbe41992accd454d641827d049d0fd03b4819710
punit <pkopensrc>
2020-02-13 15:21:41 +0530

    Fixed nginx compilation with openssl.

--- boot.sh
+++ boot.sh
@@ -1,6 +1,7 @@
-OPTS="--with-http_ssl_module"
+OPTS="--with-openssl=${OSSLSRC_DIR}"
+OPTS="$OPTS --with-http_ssl_module"
 OPTS="$OPTS --with-http_v2_module"
 OPTS="$OPTS --with-http_gzip_static_module"
 OPTS="$OPTS --with-http_gunzip_module"
-LDOPTS="-lssl -lcrypto"
+LDOPTS="${OSSLLIB_DIR}/libssl.a ${OSSLLIB_DIR}/libcrypto.a"
 ./configure ${OPTS} --with-ld-opt="${LDOPTS}" --prefix=/opt/build/nginx
